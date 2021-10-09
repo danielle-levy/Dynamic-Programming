fc03fa67b95716eab7c1f0d88504b85dc196f808
danielle-levy <danielle.levy98@gmail.com>
2021-10-09 13:58:08 +0300

    tiling problem done

--- Tiling Problem.js
+++ Tiling Problem.js
@@ -1,6 +1,32 @@
+/*
+    ABOUT THE PROBLEM:
+    counting the number of possible tiling on a board of n tiles such that we have 2 options of tiles to use:
+    - red ones that cover a single tile
+    - blue ones that cover two tiles
+*/
+
 let TilingProblemRecursive = function(n) {
+    if (n < 0) return 0
     if (n == 0 || n == 1) return 1
     return TilingProblemRecursive(n-1) + TilingProblemRecursive(n-2)
 }
 
-console.log(TilingProblemRecursive(5))
+// DP Solution: time O(n) & space O(n)
+let TilingProblem = function(n) {
+    if (n < 0) return 0
+    let dp = new Array(n + 1)
+    if (n == 0 || n == 1) {
+        dp[0] = 1
+        dp[1] = 1
+        return 1
+    }
+    if (dp[n] != null) {
+        return dp[n]
+    }
+    dp[n] = TilingProblem(n-1) + TilingProblem(n - 2)
+    return dp[n]
+}
+
+const n = 5
+console.log("Recursive Outcome: " + TilingProblemRecursive(n) + '\n' +
+    "DP Outcome: " + TilingProblem(n))
