3aac8d176b33d7cc6aec88feaf3437cfbf5ea53a
danielle-levy <danielle.levy98@gmail.com>
2021-10-16 14:53:41 +0300

    Max sub

--- Maximum Subarray.js
+++ Maximum Subarray.js
@@ -5,31 +5,34 @@
     A subarray is a contiguous part of an array.
 */
 
-
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var maxSubArray = function(nums) {
+const maxSubArray = function(nums) {
     if (nums.length < 1 || nums.length > Math.pow(10, 5)) {
         return 0
     }
 
     let dp = new Array(nums.length)
-    let max = [(0, 0), nums[0]]
+    let max = {"[0,0]": nums[0]}
     for (let i = 0; i < nums.length; i++) {
         dp[i] = new Array(nums.length)
         for (let j = i; j < nums.length; j++) {
-            if (i == j) {
+            if (i === j) {
                 dp[i][j] = nums[i]
             } else {
-                dp[i][j] = nums.slice(i, j+1).reduce((a,b)=>a+b,0);
+                dp[i][j] = dp[i][j-1] + nums[j]
                 // sums the elements in nums from index j to index i
             }
             if (dp[i][j] > max[1]) {
-                max = [(i, j), dp[i][j]]
+                max = {"[i,j]": dp[i][j]}
             }
         }
     }
-    return max[1]
+    return max
 }
+
+/*
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6
+ */
+
+console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
